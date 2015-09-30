c782465f4c2a005a21fb32e5890a36ac227a5747
David Hooey <davidhooey@gmail.com>
2015-09-30 19:43:19 -0400

    Speed tracker script.

---
+++ speed-tracker.js
@@ -0,0 +1,104 @@
+var sqlite3   = require('sqlite3').verbose();
+var speedTest = require('speedtest-net');
+var fs        = require('fs');
+var moment    = require('moment');
+var CronJob   = require('cron').CronJob;
+
+var dbFile = 'speed-tracker.db';
+var dbFileExists = fs.existsSync(dbFile);
+
+var job = new CronJob({
+    cronTime: '00,15,30,45 * * * *',
+    onTick: function() {
+        // Runs every 15 minutes
+
+        var testSpeed = speedTest({
+            maxTime:5000,
+            maxServers: 2
+        });
+
+        testSpeed.on('data', function(speedData) {
+            // {
+            //    speeds:
+            //    {
+            //      download: 6.108,
+            //      upload: 0.88
+            //    }
+            //    client:
+            //    {
+            //      ip: '23.239.38.173',
+            //      isp: 'Xplornet Communications'
+            //    }
+            //    server:
+            //    {
+            //      host: 'www.wtccommunications.ca',
+            //      location: 'Kingston, ON',
+            //      country: 'Canada',
+            //      distance: 36.44,
+            //      ping: 104.4,
+            //    }
+            // }
+
+            if(!dbFileExists) {
+                fs.openSync(dbFile, 'w');
+            }
+
+            var db = new sqlite3.Database(dbFile);
+
+            db.serialize(function() {
+                if(!dbFileExists) {
+                    db.run('create table speed_tracker \
+                            ( \
+                                id              integer primary key, \
+                                run_at          timestamp, \
+                                ping            real, \
+                                download        real, \
+                                upload          real, \
+                                client_ip       varchar(64), \
+                                client_isp      varchar(64), \
+                                server_host     varchar(64), \
+                                server_location varchar(64), \
+                                server_country  varchar(64), \
+                                server_distance real \
+                            )'
+                    );
+                }
+
+                var stmt = db.prepare('insert into speed_tracker \
+                                        ( \
+                                            run_at, \
+                                            ping, \
+                                            download, \
+                                            upload, \
+                                            client_ip, \
+                                           client_isp, \
+                                            server_host, \
+                                            server_location, \
+                                            server_country, \
+                                            server_distance \
+                                        ) \
+                                        values \
+                                            (?,?,?,?,?,?,?,?,?,?)'
+                );
+
+                stmt.run(
+                    moment().format('YYYY-MM-DD HH:mm:ss'),
+                    speedData['server']['ping'],
+                    speedData['speeds']['download'],
+                    speedData['speeds']['upload'],
+                    speedData['client']['ip'],
+                    speedData['client']['isp'],
+                    speedData['server']['host'],
+                    speedData['server']['location'],
+                    speedData['server']['country'],
+                    speedData['server']['distance']
+                );
+
+                stmt.finalize();
+            });
+
+            db.close();
+        });
+    },
+    start: true
+});
